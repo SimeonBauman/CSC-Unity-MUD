ea7fbc917d93e38b549ad9472134ebee17e86b64
SimeonBauman <96099765+SimeonBauman@users.noreply.github.com>
2023-03-29 11:41:55 -0500

    player movement

---
+++ CSC Unity MUD/Assets/Scripts/Look.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    public Transform player;
+    public float sensitivity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Mathf.Clamp(Input.mousePosition.y, -90, 90);
+        transform.localRotation = Quaternion.AngleAxis(y * sensitivity, Vector3.left);
+        player.localRotation = Quaternion.AngleAxis(Input.mousePosition.x * sensitivity, Vector3.up);
+    }
+}
---
+++ CSC Unity MUD/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public int speed = 100;
+    CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
